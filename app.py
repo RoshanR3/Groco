13ea3401d7d902b0328eb89f6d7d212c17fe14df
Krish Tiwari <114987949+Krish-tiwari212@users.noreply.github.com>
2023-06-05 21:41:16 +0530

    first commit

--- app.py
+++ app.py
@@ -8,12 +8,26 @@ from dotenv import load_dotenv
 import os
 import json
 from algoliasearch.search_client import SearchClient
+from itsdangerous import URLSafeTimedSerializer
+from flask_mail import Message, Mail
+
+
+
+
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'champion.coc212@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'champion.coc212@gmail.com'
+app.config['MAIL_PASSWORD'] = 'uvfbkvvzykelvjfy'
+
+mail = Mail(app)
 # ckeditor = CKEditor(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -28,7 +42,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    password = db.Column(db.String(1000))
     name = db.Column(db.String(1000))
 
 class Product(db.Model):
@@ -55,7 +69,26 @@ with app.app_context():
     #     db.session.commit()
     d = Product.query.all()
 
+def generate_reset_token(user):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    token = serializer.dumps(user.email, salt='password-reset')
 
+    # Save the token to the user's record in the database
+    user.reset_token = token
+    db.session.commit()
+
+    return token
+
+def send_reset_email(email, token):
+    # Construct the password reset URL using the token
+    reset_url = url_for('reset_password', token=token, _external=True)
+
+    # Create the email message
+    msg = Message('Password Reset Request', recipients=[email])
+    msg.body = f'Please click the following link to reset your password: {reset_url}'
+
+    # Send the email
+    mail.send(msg)
 
 @login_manager.unauthorized_handler
 def unauthorized():
@@ -106,6 +139,55 @@ def login():
         except AttributeError:
             return render_template("login.html", l=1)
     return render_template("login.html", l=0)
+
+
+@app.route('/forgot', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            # Generate a unique token and save it to the user's record
+            token = generate_reset_token(user)
+            # Send the password reset email containing the token
+            send_reset_email(user.email, token)
+
+        # Display a success message (without indicating if the email exists in the system)
+        return render_template('forgot.html', success=True)
+
+    return render_template('forgot.html')
+
+
+@app.route('/reset/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        # Verify and load the token to get the associated email
+        email = serializer.loads(token, salt='password-reset', max_age=3600)
+        user = User.query.filter_by(email=email).first()
+    except:
+        # Handle invalid or expired tokens
+        return render_template('reset.html', invalid=True)
+
+    if request.method == 'POST':
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+
+        if password != confirm_password:
+            return render_template('reset.html', mismatch=True)
+
+        # Update the user's password
+        user.password = generate_password_hash(password)[21:]
+        user.reset_token = None  # Clear the reset token
+        db.session.commit()
+
+        # Redirect to login page or display a success message
+        return redirect(url_for('login'))
+
+    return render_template('reset.html', token=token)
+
+
 @app.route('/logout')
 @login_required
 def logout():
